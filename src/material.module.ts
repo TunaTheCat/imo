cf355e1234624f4f3abacd74b6334c280e4914aa
Jordan Wilkinson <jordanbonnie.wilkinson@students.bfh.ch>
2023-02-23 20:38:41 +0100

    toolbar

--- src/material.module.ts
+++ src/material.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
 
     exports : [
         MatGridListModule,
-        MatCardModule
+        MatCardModule,
+        MatToolbarModule,
+        MatIconModule
     ]
 
 })
